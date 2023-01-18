05e2a2358ebbd9cb0565fd30f5094caae2b1a724
TheAntiSnipe <38178591+TheAntiSnipe@users.noreply.github.com>
2023-01-18 15:07:47 -0500

    first commit

---
+++ api/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("ELEPHANTDB_CONN_SECRET")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'My First API !!'
